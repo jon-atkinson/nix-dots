a9fcaac69dcae6c564b33593746fb6d3a27915d8
Jon Atkinson <95665780+jon-atkinson@users.noreply.github.com>
2025-10-16 16:48:57 +1100

    fix: temp update to zsh environment

--- home/shared/zsh.nix
+++ home/shared/zsh.nix
@@ -15,5 +15,11 @@
       theme = "minimal";
       plugins = [ "git" ];
     };
+    envExtra = ''
+      # Add development tools to PATH
+      export MORSE_ARM_PATH=/opt/morse/gcc-arm-none-eabi-10.3-2021.07/bin
+      export MORSE_OPENOCD_PATH=/opt/morse/xpack-openocd-0.12.0-2/bin
+      export PATH=$MORSE_ARM_PATH:$MORSE_OPENOCD_PATH:$PATH
+    '';
   };
 }
