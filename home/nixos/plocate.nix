aabecd3e207d3be070476fe3fba705dae3d53522
Jon Atkinson <95665780+jon-atkinson@users.noreply.github.com>
2025-10-18 23:36:18 +1100

    fix: correct plocate service definition

--- home/nixos/plocate.nix
+++ home/nixos/plocate.nix
@@ -1,10 +1,12 @@
-{ pkgs, ... }:
+{ pkgs, lib, ... }:
 
 {
-  home.packages = with pkgs; [
-    plocate
-  ];
-  services.plocate = {
+  home.packages =
+    with pkgs;
+    pkgs.lib.optionals pkgs.stdenv.isLinux [
+      plocate
+    ];
+  services.plocate = lib.mkIf pkgs.stdenv.isLinux {
     enable = true;
     updateInterval = "daily";
   };
