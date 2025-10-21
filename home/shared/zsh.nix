4a7bee8a0184f3df75826f257ce2a278b63d1c1c
Jon Atkinson <95665780+jon-atkinson@users.noreply.github.com>
2025-10-21 14:22:27 +1100

    Feat: add firmware development tools to repo

--- home/shared/zsh.nix
+++ home/shared/zsh.nix
@@ -38,11 +38,18 @@
       HISTORY_IGNORE="nv:ls:ll"
     '';
     envExtra = lib.mkIf (mode == "work") ''
-      # Add development tools to PATH
+      # Add MHS development tools to PATH
       export MORSE_ARM_PATH=/opt/morse/gcc-arm-none-eabi-10.3-2021.07/bin
       export MORSE_OPENOCD_PATH=/opt/morse/xpack-openocd-0.12.0-2/bin
       export PATH=$MORSE_ARM_PATH:$MORSE_OPENOCD_PATH:$PATH
       PATH=$HOME/.local/bin:$PATH
+
+      # Add Firmware development tools to environment
+      source /opt/modules/init/profile.sh
+      module switch riscv-gcc/20220921
+      module switch riscv-openocd/20230322
+      module switch gdb/14.2
+      module switch uncrustify/0.72.0
     '';
   };
 }
