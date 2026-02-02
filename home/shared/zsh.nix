a703cf33f4546c447c85873ba4e1d4976ed2ce2a
Jon Atkinson <jonathan.atkinson@vivcourt.com>
2026-02-02 18:47:10 +1100

    fix: move over vivspack config from .bashrc to .zshrc

--- home/shared/zsh.nix
+++ home/shared/zsh.nix
@@ -41,7 +41,17 @@
       alias q="rlwrap -r q"
     '';
     envExtra = lib.mkIf (mode == "work") ''
-    # DEPLOYMENT setup
+    # VIVSPACK setup
+    # enable programmable completion features (you don't need to enable
+    # this, if it's already enabled in /etc/bash.bashrc and /etc/profile
+    # sources /etc/bash.bashrc).
+    if ! shopt -oq posix; then
+      if [ -f /usr/share/bash-completion/bash_completion ]; then
+        . /usr/share/bash-completion/bash_completion
+      elif [ -f /etc/bash_completion ]; then
+        . /etc/bash_completion
+      fi
+    fi
     export VIVSPACK_ROOT=${HOME}/vivspack
 
     # NECTAR setup
